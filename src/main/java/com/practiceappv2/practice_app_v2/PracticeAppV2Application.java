95bb18e6bab3bbc323e0cf4d45e921ded8775522
Brad White <bradscottwhite@gmail.com>
2022-07-09 19:31:59 -0400

    First commit (setting up Spring boot

---
+++ src/main/java/com/practiceappv2/practice_app_v2/PracticeAppV2Application.java
@@ -0,0 +1,13 @@
+package com.practiceappv2.practice_app_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practiceappv2/practice_app_v2/PracticeAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiceappv2.practice_app_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
